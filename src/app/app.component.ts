966bd240bf6046590bdd18b37ef3bb3b64aadce7
Akanksha <akankshajadhav2999@gmail.com>
2022-09-08 14:32:30 +0530

    ADDED: Image added using property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   ngOnInit(): void {
   }
 
   title = 'Hello From Bridgelabz';
+  image = '../assets/BL_logo_square_jpg.jpg';
 }
+
+
+
