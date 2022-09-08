ebe93a142e76f6246648b2f7aad8854689a5df39
Akanksha <akankshajadhav2999@gmail.com>
2022-09-08 15:04:11 +0530

    ADDED: Used event binding for redirecting to bridgelabz's website on clicking logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,16 @@ export class AppComponent implements OnInit {
 
   title = 'Hello From Bridgelabz';
   image = '../assets/BL_logo_square_jpg.jpg';
+  url = "https://www.bridgelabz.com/";
+
+  handleClick($event: any) {
+    console.log('Clicked on bridgelabz logo');
+    alert('Redirecting to bridgelabz website !!');
+    window.open(this.url);
+  }
 }
+
+
 
 
 
