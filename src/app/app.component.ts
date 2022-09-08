d4f5dd40dfd2cf6dfd1cc4cda8cc6bc926bf36d9
Akanksha <akankshajadhav2999@gmail.com>
2022-09-08 14:09:24 +0530

    ADDED: Image added with properting binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'HelloWorld';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
+
+  title = 'Hello From Bridgelabz';
+  image =  '../assets/BL_logo_square_jpg.jpg';
 }
